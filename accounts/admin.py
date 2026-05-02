305ec721b81639c49c554383ba930e1678613c39
noorelmobashar <nooralmobasher1@gmail.com>
2026-05-02 12:24:22 +0300

    refactor: replace custom ModelAdmin classes with default admin registration across all apps

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,24 +1,4 @@
 from django.contrib import admin
 from .models import CustomUser
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'role', 'is_active', 'is_staff')
-    list_filter = ('role', 'is_active', 'is_staff', 'is_superuser', 'groups')
-    search_fields = ('username', 'email', 'first_name', 'last_name', 'phone_number')
-    ordering = ('username',)
-    filter_horizontal = ('groups', 'user_permissions')
-    fieldsets = (
-        (None, {
-            'fields': ('username', 'password')
-        }),
-        ('Personal info', {
-            'fields': ('first_name', 'last_name', 'email', 'phone_number')
-        }),
-        ('Permissions', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
-        }),
-        ('Important dates', {
-            'fields': ('last_login', 'date_joined')
-        }),
-    )
+admin.site.register(CustomUser)
--- reception/admin.py
+++ reception/admin.py
@@ -1,16 +1,4 @@
 from django.contrib import admin
 from .models import WalkInPatient
 
-@admin.register(WalkInPatient)
-class WalkInPatientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone_number', 'notes', 'created_at')
-    list_filter = ('created_at',)
-    search_fields = ('name', 'phone_number')
-    readonly_fields = ('created_at',)
-    
-    # Filter by doctor by default
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.role == 'DOCTOR':
-            return qs.filter(patient__doctor=request.user)
-        return qs
+admin.site.register(WalkInPatient)
