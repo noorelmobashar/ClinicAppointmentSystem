03b3082545317ad3e6a4007cc0cf65963a9c4b85
noorelmobashar <nooralmobasher1@gmail.com>
2026-05-02 14:23:22 +0300

    feat: register DoctorSchedule model in admin panel

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Appointment, AppointmentSlot
+from .models import Appointment, AppointmentSlot, DoctorSchedule
 
 admin.site.register(Appointment)
 admin.site.register(AppointmentSlot)
+admin.site.register(DoctorSchedule)
